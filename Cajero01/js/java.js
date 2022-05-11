f4a3533ede985260fc4cd8807d4bf60cb776792c
Nicolle27 <103013970+Nicolle27@users.noreply.github.com>
2022-05-11 15:17:02 -0500

    Add files via upload

---
+++ Cajero01/js/java.js
@@ -0,0 +1,14 @@
+function ingresar(){
+    let password = "27122002";
+    var p = document.getElementById("contra").value;
+
+    
+
+    if(p == password){
+        document.getElementById("container2").style.display="block";
+        document.getElementById("container").style.display="none";
+
+    }
+   
+    
+}
